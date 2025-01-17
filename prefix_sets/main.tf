c440eb130918527ee4a13ef9073f492872b340ce
Sarvesh Wagh <sarveshwagh93@gmail.com>
2025-01-17 18:28:58 +0100

    4th commit

---
+++ prefix_sets/main.tf
@@ -0,0 +1,12 @@
+resource "volterra_ip_prefix_set" "name" {
+  name = var.prefixsetname
+  namespace = var.mynamespace
+  ipv4_prefixes {
+    ipv4_prefix = "1.2.3.4/24"
+    description = "ips with know bad reputation history"
+  }
+#   ipv6_prefixes {
+#     ipv6_prefix = "2001:db8::/32"
+#     description = "ips with know bad reputation history"
+#   }
+}
---
+++ prefix_sets/terraform.tfvars
@@ -0,0 +1,4 @@
+mynamespace = "wagh"
+prefixsetname = "sarvesh-prefix-set"
+api_p12_file      = "xxxx"
+api_url           = "xxxx"

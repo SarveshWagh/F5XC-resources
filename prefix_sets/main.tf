fbde191c4efeb037b29ba285a8944e32546b53e0
Sarvesh Wagh <sarveshwagh93@gmail.com>
2025-01-23 13:36:10 +0100

    prefix set 5th commit

--- prefix_sets/main.tf
+++ prefix_sets/main.tf
@@ -5,6 +5,10 @@ resource "volterra_ip_prefix_set" "name" {
     ipv4_prefix = "1.2.3.4/24"
     description = "ips with know bad reputation history"
   }
+  ipv4_prefixes {
+    ipv4_prefix = "1.2.3.5/24"
+    description = "ips with know bad reputation history"
+  }
 #   ipv6_prefixes {
 #     ipv6_prefix = "2001:db8::/32"
 #     description = "ips with know bad reputation history"

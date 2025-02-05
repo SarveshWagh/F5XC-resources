252a54229fa065caa2609b20f0dfb6d4c438d495
Sarvesh Wagh <sarveshwagh93@gmail.com>
2025-02-05 10:39:20 +0100

    primary dns zone commit 2

--- primary_dns_zone/main.tf
+++ primary_dns_zone/main.tf
@@ -1,3 +1,7 @@
+# Please note that this terraform code is for primary DNS zone with automatic SSL certificates. If there is a case, 
+# where you would like to use the DNS zones with customer SSL certificates, we can work on the same. 
+# The DNS zone will automatically get updated when we onboard new apps to the HTTP LB thats delegated to XC.
+# The records get added to "Additional Resource Record Sets" and its encouraged to add more to this section rather than the default "Resource Record Sets"
 resource "volterra_dns_zone" "name" {
   name = var.dns_zone_name
   namespace = var.mynamespace
